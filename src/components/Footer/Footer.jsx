41428493150e3d59b836f4c07c7a57f14181d25f
Oliver Graovac <olivergraovac@gmail.com>
2021-08-21 11:41:16 +0200

    Added data as json and js files and created Header and Footer components

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => (
+  <footer class="page-footer">
+    <div class="footer-copyright">
+      <div class="container">© 2021 Copyright Text</div>
+    </div>
+  </footer>
+);
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#!" className="brand-logo">
+          React Users
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <a class="waves-effect waves-light btn">Button</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
